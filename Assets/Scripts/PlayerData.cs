ee5b09dcfa3bea3c6a87738bd85544a76f50486c
maxdk9 <maxdk9@gmail.com>
2019-09-04 17:47:19 +0300

    playerstruct

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public struct PlayerData : IComponentData
+{
+	public int Hp;
+	public int Coins;
+}
